704f2fd9c44174045b05f4b9144c3358b03791c9
Rostyslav Bets <rostyslawbets@gmail.com>
2026-05-14 20:07:02 +0200

    new file

---
+++ main.py
@@ -0,0 +1,25 @@
+# def write1():
+#     x = input("Напиши що робити в інфінітиві: ")
+#     newlist = []
+#     if x.lower() == "стоп" or x.lower() == "stop":
+#         print("Програма завершена.")
+#         return newlist
+#     else:
+#         newlist.append(x)
+#         write1()
+    
+# print(write1())
+newlist = []
+def validation(text):
+    if text.lower() == "стоп" or text.lower() == "stop":
+        print("Програма завершена.")
+        return False
+    return True
+
+while True:
+    text = input("Напиши що робити в інфінітиві: ")
+    if not validation(text):
+        break
+    newlist.append(text)
+
+print(newlist)
